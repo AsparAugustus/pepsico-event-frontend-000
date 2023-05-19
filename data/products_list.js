74b1fff4e48cc9b82e8a7c76ae16798e2b8921e6
Merlin <50379729+AsparAugustus@users.noreply.github.com>
2023-05-19 18:26:41 +0100

    final edition ready for event

--- data/products_list.js
+++ data/products_list.js
@@ -248,6 +248,474 @@ export default {
 
 
 
+      {
+        "product_id": "38",
+        "name": "Go Grain Crunchies",
+        "category": "Diverse Ingredients",
+        "img": "diverse_00.jpg"
+      },
+      {
+        "product_id": "39",
+        "name": "Saffola Munchiez ",
+        "category": "Diverse Ingredients",
+        "img": "diverse_01.jpg"
+      },
+      {
+        "product_id": "40",
+        "name": "Monsoon Harvest Toasted Millet Muesli ",
+        "category": "Diverse Ingredients",
+        "img": "diverse_02.jpg"
+      },
+      {
+        "product_id": "41",
+        "name": "100% Plant Protein Pro Muesli",
+        "category": "Diverse Ingredients",
+        "img": "diverse_03.jpg"
+      },
+      {
+        "product_id": "42",
+        "name": "Millet Pongal",
+        "category": "Diverse Ingredients",
+        "img": "diverse_04.jpg"
+      },
+      {
+        "product_id": "43",
+        "name": "Naan Chips",
+        "category": "Diverse Ingredients",
+        "img": "diverse_05.jpg"
+      },
+      {
+        "product_id": "44",
+        "name": "Organic Quinoa Power Rice Cake",
+        "category": "Diverse Ingredients",
+        "img": "diverse_06.jpg"
+      },
+      {
+        "product_id": "45",
+        "name": "GoodDiet Brown Rice Chips With Oats",
+        "category": "Diverse Ingredients",
+        "img": "diverse_07.jpg"
+      },
+      {
+        "product_id": "46",
+        "name": "Saffola Munchiez Roasted Makhana",
+        "category": "Diverse Ingredients",
+        "img": "diverse_08.jpg"
+      },
+      {
+        "product_id": "47",
+        "name": "Crunchy Butter Murukku",
+        "category": "Diverse Ingredients",
+        "img": "diverse_09.jpg"
+      },
+      {
+        "product_id": "48",
+        "name": "PVR 4700 BC Protein Pops",
+        "category": "Diverse Ingredients",
+        "img": "diverse_10.jpg"
+      },
+      {
+        "product_id": "49",
+        "name": "Sundrop Snacko Sweet Corn",
+        "category": "Diverse Ingredients",
+        "img": "diverse_11.jpg"
+      },
+      {
+        "product_id": "50",
+        "name": "Rajdhani Makhana ",
+        "category": "Diverse Ingredients",
+        "img": "diverse_12.jpg"
+      },
+
+
+
+
+
+
+
+
+
+
+
+      {
+        "product_id": "51",
+        "name": "Yogabar Oats Veggies Masala ",
+        "category": "Core Expansion",
+        "img": "core_00.jpg"
+      },
+      {
+        "product_id": "52",
+        "name": "Saffola Karara Crunch",
+        "category": "Core Expansion",
+        "img": "core_01.jpg"
+      },
+      {
+        "product_id": "53",
+        "name": "Bingo Hashtags",
+        "category": "Core Expansion",
+        "img": "core_02.jpg"
+      },
+      {
+        "product_id": "54",
+        "name": "Bingo Nachos",
+        "category": "Core Expansion",
+        "img": "core_03.jpg"
+      },
+      {
+        "product_id": "55",
+        "name": "ITC Bingo Original Style Salt & Chili Sprinkled Potato Chips",
+        "category": "Core Expansion",
+        "img": "core_04.jpg"
+      },
+      {
+        "product_id": "56",
+        "name": "Sundrop Instant Rolled Oats",
+        "category": "Core Expansion",
+        "img": "core_05.jpg"
+      },
+      {
+        "product_id": "57",
+        "name": "Limited Edition Assortment Pringles",
+        "category": "Core Expansion",
+        "img": "core_06.jpg"
+      },
+      {
+        "product_id": "58",
+        "name": "Fast & Up Good Eats instant Flavour oats",
+        "category": "Core Expansion",
+        "img": "core_07.jpg"
+      },
+      {
+        "product_id": "59",
+        "name": "Crax Potato Chips ",
+        "category": "Core Expansion",
+        "img": "core_08.jpg"
+      },
+      {
+        "product_id": "60",
+        "name": "Cornitos Nacho Chips - Sweet Chili",
+        "category": "Core Expansion",
+        "img": "core_09.jpg"
+      },
+      {
+        "product_id": "61",
+        "name": "Haldiram's Smart Oats",
+        "category": "Core Expansion",
+        "img": "core_10.jpg"
+      },
+      {
+        "product_id": "62",
+        "name": "Monsoon Harvest Jumbo Rolled Oats",
+        "category": "Core Expansion",
+        "img": "core_11.jpg"
+      },
+      {
+        "product_id": "63",
+        "name": "Yogabar 100% Steel Cut Oats",
+        "category": "Core Expansion",
+        "img": "core_12.jpg"
+      },
+      {
+        "product_id": "64",
+        "name": "First Cropt Cream N Onion Potato Chips",
+        "category": "Core Expansion",
+        "img": "core_13.jpg"
+      },
+      {
+        "product_id": "65",
+        "name": "Yoga Bar Fruits plus nut seeds ",
+        "category": "Core Expansion",
+        "img": "core_14.jpg"
+      },
+      {
+        "product_id": "66",
+        "name": "First Crop Oats",
+        "category": "Core Expansion",
+        "img": "core_15.jpg"
+      },
+      {
+        "product_id": "67",
+        "name": "Bikano Salted Wafers",
+        "category": "Core Expansion",
+        "img": "core_16.jpg"
+      },
+      {
+        "product_id": "68",
+        "name": "Makino Nacho Set ",
+        "category": "Core Expansion",
+        "img": "core_17.jpg"
+      },
+      {
+        "product_id": "69",
+        "name": "Parle FullToss",
+        "category": "Core Expansion",
+        "img": "core_18.jpg"
+      },
+      {
+        "product_id": "70",
+        "name": "cornitos cheese and herbs; cornitos tomato mexicana ",
+        "category": "Core Expansion",
+        "img": "core_19.jpg"
+      },
+      {
+        "product_id": "71",
+        "name": "Baggry's Oats Atta ",
+        "category": "Core Expansion",
+        "img": "core_20.jpg"
+      },
+      {
+        "product_id": "72",
+        "name": "Haldiram's future life oats atta ",
+        "category": "Core Expansion",
+        "img": "core_21.jpg"
+      },
+      {
+        "product_id": "73",
+        "name": "Oat Bran Hi-Fibre Delicious Cereal",
+        "category": "Core Expansion",
+        "img": "core_22.jpg"
+      },
+
+
+
+
+
+
+
+
+
+    {
+      "product_id": "74",
+      "name": "Cornado Lentil Twisters",
+      "category": "Flavour",
+      "img": "flavour_00.jpg"
+    },
+    {
+      "product_id": "75",
+      "name": "Bingo Street Bites Dahi Chaat remix",
+      "category": "Flavour",
+      "img": "flavour_01.jpg"
+    },
+    {
+      "product_id": "76",
+      "name": "MB Dark Chocolate Muesli Munchies",
+      "category": "Flavour",
+      "img": "flavour_02.jpg"
+    },
+    {
+      "product_id": "77",
+      "name": "Britannia 50 50 Potazos Crisps",
+      "category": "Flavour",
+      "img": "flavour_03.jpg"
+    },
+    {
+      "product_id": "78",
+      "name": "Flaveo Grain Chips",
+      "category": "Flavour",
+      "img": "flavour_04.jpg"
+    },
+    {
+      "product_id": "79",
+      "name": "Dark Chocolate Flavoured High Protein Oats with Probiotic",
+      "category": "Flavour",
+      "img": "flavour_05.jpg"
+    },
+    {
+      "product_id": "80",
+      "name": "Parle Piri Piri Potato Chips",
+      "category": "Flavour",
+      "img": "flavour_06.jpg"
+    },
+    {
+      "product_id": "81",
+      "name": "Crunchy Dark Choco Berry Muesli",
+      "category": "Flavour",
+      "img": "flavour_07.jpg"
+    },
+    {
+      "product_id": "82",
+      "name": "Beer 'n Barbeque Popped Potato Chips",
+      "category": "Flavour",
+      "img": "flavour_08.jpg"
+    },
+    {
+      "product_id": "83",
+      "name": "Orion Turtle Chips",
+      "category": "Flavour",
+      "img": "flavour_09.jpg"
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    {
+      "product_id": "84",
+      "name": "Kettle Wafers",
+      "category": "Premium",
+      "img": "premium_00.jpg"
+    },
+    {
+      "product_id": "85",
+      "name": "Black Truffle Hand Cooked Potato Chips",
+      "category": "Premium",
+      "img": "premium_01.jpg"
+    },
+    {
+      "product_id": "86",
+      "name": "Rice Popped Chips ",
+      "category": "Premium",
+      "img": "premium_02.jpg"
+    },
+    {
+      "product_id": "87",
+      "name": "Jacob's Mini Cheddar",
+      "category": "Premium",
+      "img": "premium_03.jpg"
+    },
+    {
+      "product_id": "88",
+      "name": "Dark Chocolate & Espresso Crunchy Granola Bars",
+      "category": "Premium",
+      "img": "premium_04.jpg"
+    },
+    {
+      "product_id": "89",
+      "name": "4700BC  Popped Corn Chips +",
+      "category": "Premium",
+      "img": "premium_05.jpg"
+    },
+    {
+      "product_id": "90",
+      "name": "4700 BC Corns",
+      "category": "Premium",
+      "img": "premium_06.jpg"
+    },
+    {
+      "product_id": "91",
+      "name": "Why Fryy Potato Chips",
+      "category": "Premium",
+      "img": "premium_07.jpg"
+    },
+    {
+      "product_id": "92",
+      "name": "Haldiram Peeloz ",
+      "category": "Premium",
+      "img": "premium_08.jpg"
+    },
+    {
+      "product_id": "93",
+      "name": "Multigrain Munchiez ",
+      "category": "Premium",
+      "img": "premium_09.jpg"
+    },
+    {
+      "product_id": "94",
+      "name": "Swiss Style Museli",
+      "category": "Premium",
+      "img": "premium_10.jpg"
+    },
+    {
+      "product_id": "95",
+      "name": "Chocolate Whey Protein Muesli with Almonds",
+      "category": "Premium",
+      "img": "premium_11.jpg"
+    },
+    {
+      "product_id": "96",
+      "name": "Strawberry Fills Finger Millet Bites",
+      "category": "Premium",
+      "img": "premium_12.jpg"
+    },
+
+
+
+
+
+
+
+
+
+
+    {
+      "product_id": "97",
+      "name": "Fun Flips Chinese Puffs",
+      "category": "Affordable",
+      "img": "affordable_00.jpg"
+    },
+    {
+      "product_id": "98",
+      "name": "Fun flips party pack",
+      "category": "Affordable",
+      "img": "affordable_01.jpg"
+    },
+    {
+      "product_id": "99",
+      "name": "Pepitos Corn Puffs",
+      "category": "Affordable",
+      "img": "affordable_02.jpg"
+    },
+    {
+      "product_id": "100",
+      "name": "Haldiram's Puffcorns ",
+      "category": "Affordable",
+      "img": "affordable_03.jpg"
+    },
+    {
+      "product_id": "101",
+      "name": "Peppy Variety Pack",
+      "category": "Affordable",
+      "img": "affordable_04.jpg"
+    },
+    {
+      "product_id": "102",
+      "name": "Crax Curls Chatpata Masala",
+      "category": "Affordable",
+      "img": "affordable_05.jpg"
+    },
+    {
+      "product_id": "103",
+      "name": "Crax Ring Masala Mania",
+      "category": "Affordable",
+      "img": "affordable_06.jpg"
+    },
+    {
+      "product_id": "104",
+      "name": "Crax Balls ",
+      "category": "Affordable",
+      "img": "affordable_07.jpg"
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
